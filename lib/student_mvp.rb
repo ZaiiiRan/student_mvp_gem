79cace49c050f95c566d4425dccb9bc1a3f5444a
ZaiiiRan <a1b075753@gmail.com>
2024-12-02 20:51:45 +0300

    add requirements ti student_mvp.rb

--- lib/student_mvp.rb
+++ lib/student_mvp.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 require_relative "student_mvp/version"
+require_relative './presenters/base_presenters/base_presenter.rb'
+require_relative './presenters/base_presenters/student_list_presenter.rb'
+require_relative './presenters/edit_student/add_student_presenter.rb'
+require_relative './presenters/edit_student/edit_contacts_presenter.rb'
+require_relative './presenters/edit_student/edit_git_presenter.rb'
+require_relative './presenters/edit_student/edit_student_presenter.rb'
+require_relative './presenters/edit_student/replace_student_presenter.rb'
+require_relative './data_access/DB_client/DB_client.rb'
 
 module StudentMvp
   class Error < StandardError; end
