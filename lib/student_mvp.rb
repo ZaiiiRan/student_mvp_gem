e29d41e010d9f8c1182d99bf8b0979dde5ccfadc
ZaiiiRan <a1b075753@gmail.com>
2024-12-02 22:06:59 +0300

    add logger to require

--- lib/student_mvp.rb
+++ lib/student_mvp.rb
@@ -9,6 +9,7 @@ require_relative './presenters/edit_student/edit_git_presenter.rb'
 require_relative './presenters/edit_student/edit_student_presenter.rb'
 require_relative './presenters/edit_student/replace_student_presenter.rb'
 require_relative './data_access/DB_client/DB_client.rb'
+require_relative './logger/logger.rb'
 
 module StudentMvp
   class Error < StandardError; end
